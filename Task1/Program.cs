95bdd2808d11e40255da31cfacae56167c4b24b7
AlexUnder <kn191_mos@student.ztu.edu.ua>
2022-11-08 22:41:55 +0200

    Changed first 3 lines in Program.cs

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+Console.WriteLine("Sana Commerce Software Development Bootcamp");
+Console.WriteLine("Курси Sana Software Development Bootcamp");
+Console.WriteLine("Курси Sana Commerce Software Bootcamp");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
