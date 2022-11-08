c0cfe0cfa49104b78a36c78e9d148cb69bffebe1
AlexUnder <kn191_mos@student.ztu.edu.ua>
2022-11-08 22:45:07 +0200

    Changed last 2 lines in Program.cs

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -3,7 +3,7 @@ Console.WriteLine("Sana Commerce Software Development Bootcamp");
 Console.WriteLine("Курси Sana Software Development Bootcamp");
 Console.WriteLine("Курси Sana Commerce Software Bootcamp");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+Console.WriteLine("Hello World!");
+Console.WriteLine("Hello Oleksandr!");
 
 
